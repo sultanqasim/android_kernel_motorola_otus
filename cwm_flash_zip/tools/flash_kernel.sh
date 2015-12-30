d1983ab4db9a05e58ee07eb547725701fa47f5b1
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-12-29 23:19:05 -0500

    cwm_flash_zip: remove utags from kernel command line
    We no longer use them on CM13.

--- cwm_flash_zip/tools/flash_kernel.sh
+++ cwm_flash_zip/tools/flash_kernel.sh
@@ -16,5 +16,5 @@
 cd /tmp/
 /sbin/busybox dd if=/dev/block/platform/msm_sdcc.1/by-name/boot of=./boot.img
 ./unpackbootimg -i /tmp/boot.img
-./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags vmalloc=400M androidboot.write_protect=0"  --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt /tmp/dt.img -o /tmp/newboot.img
+./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x37 vmalloc=400M"  --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt /tmp/dt.img -o /tmp/newboot.img
 /sbin/busybox dd if=/tmp/newboot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
